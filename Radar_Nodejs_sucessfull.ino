f803a5a95bf673020fa25e7c3f8135f3811a3819
SAWADOGO Rimsongdo Roger <152446630+Rimsongdo@users.noreply.github.com>
2024-05-02 23:48:38 +0100

    Update Radar_Nodejs_sucessfull.ino

--- Radar_Nodejs_sucessfull.ino
+++ Radar_Nodejs_sucessfull.ino
@@ -1,31 +1,26 @@
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
-#include <ESP8266HTTPClient.h> // Nouvelle bibliothèque pour les requêtes HTTP POST
+#include <ESP8266HTTPClient.h> 
 #include <Servo.h>
 char ssid[] = "INPT-TEST";         // SSID du réseau Wi-Fi
 char password[] = "iinnpptt"; // Mot de passe du réseau Wi-Fi
-// Defines Tirg and Echo pins of the Ultrasonic Sensor
+
 const int trigPin = 4;
 const int echoPin = 2;
 int leftAngle=180;
 int rightAngle=0;
-// Variables for the duration and the distance
+
 int i=rightAngle;
 float distance;
+
 float mesurerDistance() {
-  // Déclencher une impulsion ultrasonique
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
-
-  // Mesurer la durée de l'impulsion ultrasonique
   long duration = pulseIn(echoPin, HIGH);
-
-  // Convertir la durée en distance en centimètres
   float distance_cm = duration * 0.034 / 2;
-
   return distance_cm; // Retourner la distance mesurée
 }
 
@@ -35,13 +30,13 @@ const uint16_t serverPort = 80; // Port sur lequel le serveur Node.js écoute
 
 WiFiClient client;
 
-Servo myServo; // Creates a servo object for controlling the servo motor
+Servo myServo;
+
 void setup() {
- pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
-  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+ pinMode(trigPin, OUTPUT); 
+  pinMode(echoPin, INPUT); 
   Serial.begin(115200);
   myServo.attach(5);
-  
   WiFi.mode(WIFI_STA);
   WiFi.disconnect();
   delay(100);
@@ -64,13 +59,8 @@ void makeHTTPRequest() {
     return;
   }
 
-  // Génération d'une température aléatoire entre 0 et 100
- 
-  // Construction du corps de la requête POST avec la température
+
   String postData = "angg=" + String(i) + "&diss=" + String(distance);
-
-
-  // Envoi de la requête POST
   client.println("POST /data HTTP/1.1");
   client.print("Host: ");
   client.println(serverIP.toString());
@@ -80,29 +70,20 @@ void makeHTTPRequest() {
   client.println(postData.length());
   client.println();
   client.println(postData);
-
-  // Attendre un court instant
   delay(100);
-
-  // Lecture de la réponse du serveur
   while (client.available()) {
     char c = client.read();
     Serial.print(c);
   }
-
-  // Fermeture de la connexion
   client.stop();
 }
 
 void loop() {
   for( i=rightAngle;i<=leftAngle;i++){
          makeHTTPRequest();
-      
-     
-  myServo.write(i);
-  
-  delay(10);
-  distance = mesurerDistance();
+        myServo.write(i);
+        delay(10);
+        distance = mesurerDistance();
     }
    
   for( i=leftAngle;i>rightAngle;i--){
@@ -111,8 +92,5 @@ void loop() {
         myServo.write(i);
         delay(10);
         distance = mesurerDistance();
-  
-  }
- 
- 
+   } 
 }
